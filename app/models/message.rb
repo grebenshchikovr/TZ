90a95b50d9e1a89eea995fae4d77ecea000e0de5
grebenshchikovr <grebenshchikov.r@gmail.com>
2017-11-12 23:44:17 +0300

    delete useless fields and fix root route

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ApplicationRecord
   belongs_to :sender, :foreign_key => :sender_id, class_name: 'User'
   belongs_to :reciver, :foreign_key => :receiver_id, class_name: 'User'
 
-  validates_presence_of :message, :sender_id, :receiver_id
+  validates_presence_of :message, :sender_id, :receiver_id, :message => "can't be empty"
 end
